44bedc5efc37c70d41dfc6bae8bc977dc8e9d5ab
Dania-Abuhijleh <abuhijleh.d@northeastern.edu>
2021-08-19 16:16:10 +0300

    Update main.py

--- fall-2021/project/main.py
+++ fall-2021/project/main.py
@@ -6,8 +6,8 @@ from z3 import *
 import string
 
 global listOfOperators
-global EQ_OPERATORS
-EQ_OPERATORS = ['=', '>', '<', '≤', '≥']
+# global EQ_OPERATORS
+# EQ_OPERATORS = ['=', '>', '<', '≤', '≥']
 s = Solver()
 
 import sys
@@ -127,11 +127,11 @@ def getFormula(model, z3_exp):
     if z3_exp != None:
         leftF = getFormula(model, getLeftChild(z3_exp))
         rootVal = z3_exp.decl()
-        if str(rootVal) == 'Int':
+        if str(rootVal) == 'Int' or len(z3_exp.children()) == 0: # or is_int(rootVal) or is_real(rootVal) or is_bool(rootVal):
             rootVal = z3_exp
         global listOfOperators
         matched = False
-        for index,listOp in enumerate(listOfOperators):
+        for listOp in listOfOperators:
             if str(rootVal) in listOp:
                 idx = model[(var[modelIdx])].as_long()
                 rv = listOp[idx]
@@ -176,6 +176,12 @@ def joinFormulasZ3(op, leftF, rightF):
             formula = leftF <= rightF
         elif op == '!=':
             formula = leftF != rightF
+        elif op == '=':
+            formula = leftF != rightF
+        elif op == '**':
+            formula = leftF ** rightF
+        elif op == '%':
+            formula = leftF % rightF
     return formula
 
 
@@ -186,7 +192,7 @@ def getFormulasFormatted(flist):
         valid = checkValid(z3_exp)
         sat = checkSat(z3_exp)
         #diff = diffList[index]
-        result.append(" ".join([z3_exp, valid, sat])) #, str(diff)]))
+        result.append(" ".join([str(z3_exp), valid, sat])) #, str(diff)]))
     return result
 
 def checkSat(z3_exp):
@@ -205,3 +211,51 @@ exp1 = '(declare-const x Int) (assert (=> (> x 1) (> x 0)))'
 
 mainResult(exp1, [['Implies', '=='], ['>', '<','=']])
 # maybe I can make this a string then bring it back to z3? How?
+
+z3_exp = parse_smt2_string(exp1)[0]
+x1 = z3_exp.children()[0].children()[0]
+one1 = z3_exp.children()[0].children()[1]
+#formula = x1 > one1
+print(len(x1.children()))
+print(len(one1.children()))
+print(is_bool(Bool('P')))
+print(is_bool(Or(Bool('P'), Bool('Q'))))
+#print(formula)
+
+# exp2 = Implies(Int('x') > 1, Int('x') > 0)
+# v = parse_smt2_string('(declare-const x Int) (assert (=> (> x 1) (> x 0)))')
+# x1 = v[0].children()[0].children()[0]
+# one1 = v[0].children()[0].children()[1]
+# print(x1 > one1)
+# print('howdy')
+# print(exp2.children())
+# print(exp2.decl())
+# print(v[0])
+# testList = [And, Or, Implies]
+# varList = [Bool('P'), Bool('Q')]
+# print(testList[0](varList[0], varList[1]))
+# exp5 = Not(Bool('P'))
+# #print(Bool('R')(None, None))
+# #exp6 = And(Bool('P'))
+# x = Int('x')
+# exp7 = (x > 1) == (x > 0)
+# print(checkSat(exp7))
+# import operator
+# print(operator.gt(2,4))
+
+
+# import sys
+# current_module = sys.modules['z3']
+# #print(sys.modules) 
+# # z3_exp1 = getattr(current_module, 'Implies')(x > 1, x > 2)
+# # z3_exp1 = getattr(current_module, 'gt')(x, 2)
+# # print(z3_exp1)
+# one = 1
+# #print(one.decl())
+# ast1 = exp7.children()[0].children()[0]
+# print(eq(ast1, Int('x')))
+# exp8 = x > 1
+# so = Solver()
+# #so.add(exp8)
+# so.add(x1 > one1)
+# print(so.check())
